78c67ac59bda3d063df63d55357dc3f7a8a0bd27
Frank Bosman <fbushman@gmail.com>
2025-01-28 16:11:16 +0100

    Fixed condition creator functions

--- Models/Enum/Condition.lua
+++ Models/Enum/Condition.lua
@@ -1,10 +1,14 @@
 Condition = {
     CastCount = {
         name = "Cast Count",
-        creator = CastCountCondition.New
+        creator = function ()
+            return CastCountCondition:New()
+        end
     },
     UnitHealth = {
         name = "Unit Health",
-        creator = UnitHealthCondition.New
+        creator = function ()
+            return UnitHealthCondition:New()
+        end
     }
 }
